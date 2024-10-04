f103fa0921b5d9cc7a69ed55006eb77dceac1a7b
Hyeonchang Jeon <kevinjeon119@gmail.com>
2024-10-04 20:59:56 +0900

    Update Intervention & Opimize IL (#10)
    * Add stacking obs
    
    * Make IL model
    
    * Optimize IL
    
    * Improve IL
    
    * Change inervention.py
    
    * Update inervention analyzing

--- algorithms/il/analyze/intervention.py
+++ algorithms/il/analyze/intervention.py
@@ -25,59 +25,6 @@ def parse_args():
     parser.add_argument('--num-stack', '-s', type=int, default=5)
     args = parser.parse_args()
     return args
-def change_obs_prime(obs, veh_ind, features='partner'):
-
-    # todo: make variables for getting specific chagnes of values
-    if features == 'ego':
-        obs_prime = change_ego_state(obs) # this chnges the ego state value in reasonable boundary.
-
-    elif features == 'partner':
-        obs_prime = change_partner_state(obs, veh_ind) # this chnges the ego state value in reasonable boundary.
-
-    else:
-        obs_prime = change_edge_state(obs) # this chnges the ego state value in reasonable boundary.
-
-    return obs_prime
-
-def change_ego_state(obs):
-    '''
-    Define the reasonable bound of ego
-    - veh speed
-    - veh length
-    - veh width
-    - relative goal pos
-    - collision or not
-    '''
-
-    pass
-
-def change_partner_state(obs, veh_ind, feat_ind=0, delta=0.1):
-    '''
-    obs: (num_world, num_veh, num_partner, partner_feat)
-    delta: (N, )
-    Define the reasonable bound of partner
-    (num_control, 10)
-    - speed 1
-    - relative pos 2
-    - relative orienttaion 1
-    - length 1
-    - width 1
-    - type
-    '''
-    partner_obs = obs[:, :, 6:1276]
-
-    obs_prime = partner_obs.reshape(-1, 128, 127, 10).clone()
-
-    if feat_ind == 0:
-        obs_prime[:, :, veh_ind, 0] += delta
-        obs_prime[:, :, veh_ind, 0] = torch.clamp(obs_prime[:, :, veh_ind, 0], 0, 1)
-
-    elif feat_ind == 1:
-        obs_prime[:, veh_ind, 1:3] += delta
-        obs_prime[:, veh_ind, 1:3] = torch.clamp(obs_prime[:, veh_ind, 1:3], -1, 1)
-    obs_prime = obs_prime.reshape(-1, 128, 1270)
-    obs[:, :, 6:1276] = obs_prime
-    return obs
 
 def visualize_heatmap(diffs, delta):
 
@@ -108,13 +55,35 @@ def visualize_heatmap(diffs, delta):
     plt.show()
 
 
+def change_partner_state(obs, veh_ind, deltas, feat_size):
+    '''
+    obs: (num_world, num_veh, num_partner, partner_feat)
+    '''
+    obs = obs.repeat(20, 1, 1)
+    for i in range(5):  # todo: should change for num_stack variable
+        partner_obs = obs[:, :, i * feat_size + 6:i * feat_size + 1276]
 
-def change_edge_state(obs):
-    pass
+        obs_prime = partner_obs.reshape(-1, 128, 127, 10).clone()
+
+        # Expand obs_prime along the second dimension to match deltas
+        # obs_prime = obs_prime.unsqueeze(1).repeat(1, len(deltas), 1, 1, 1)
+        # Expand deltas to match the obs_prime dimensions
+        deltas_expanded = deltas.view(len(deltas), 1).to(obs_prime.device)
+        deltas_expanded = deltas_expanded.repeat(1, 128)
+        # Adjust the delta along the vehicle index
+        obs_prime[:, :, veh_ind, 0] += deltas_expanded
+        obs_prime[:, :, veh_ind, 0] = torch.clamp(obs_prime[:, :, veh_ind, 0], 0, 1)
+
+        # Reshape back to the original shape
+        obs_prime = obs_prime.reshape(-1, len(deltas), 128, 1270)
+        obs[:, :, i * feat_size + 6:i * feat_size + 1276] = obs_prime
+
+    return obs
+
 
 if __name__ == "__main__":
     args = parse_args()
-    # Configurations
+
     # Configurations
     env_config = EnvConfig(
         dynamics_model=args.dynamics_model,
@@ -138,24 +107,28 @@ if __name__ == "__main__":
     bc_config = BehavCloningConfig()
 
     NUM_WORLDS = 1
+    feat_size = 3876
+
     scene_config = SceneConfig(f"/data/formatted_json_v2_no_tl_train/", NUM_WORLDS)
-    # print('Initializeing env....')
     env = GPUDriveTorchEnv(
         config=env_config,
         scene_config=scene_config,
-        max_cont_agents=1,  # Number of agents to control
+        max_cont_agents=1,
         device=args.device,
         render_config=render_config,
         action_type='continuous',
         num_stack=args.num_stack
     )
+
     bc_policy = torch.load(f"{bc_config.model_path}/{args.model_name}.pth").to(args.device)
     bc_policy.eval()
+
     alive_agent_mask = env.cont_agent_mask.clone()
     obs = env.reset()
     frames = []
-    deltas = torch.linspace(-0.5, 0.5, 20)
+    deltas = torch.linspace(-0.5, 0.5, 20).to(args.device)
     diffs = torch.zeros(NUM_WORLDS, 91, 127).to(args.device)
+
     for time_step in range(env.episode_len):
         with torch.no_grad():
             actions = bc_policy(obs, deterministic=True)
@@ -163,15 +136,16 @@ if __name__ == "__main__":
             action_deltas = torch.zeros(len(deltas))
             for i, delta in enumerate(deltas):
                 with torch.no_grad():
-                    obs_prime = change_partner_state(obs, veh_ind=veh_ind, delta=delta)
+                    obs_prime = change_partner_state(obs, veh_ind=veh_ind, deltas=deltas,
+                                                     feat_size=feat_size)
                     actions_prime = bc_policy(obs_prime, deterministic=True)
                     action_deltas[i] = abs(actions - actions_prime).sum() / (3 * args.action_scale)
             diff = action_deltas.mean()
             diffs[:, time_step, veh_ind] = diff
-        env.step_dynamics(actions / args.action_scale)
 
+        env.step_dynamics(actions / args.action_scale)
         obs = env.get_obs()
         dones = env.get_dones()
         infos = env.get_infos()
-    visualize_heatmap(diffs[0], delta)
 
+    visualize_heatmap(diffs[0], deltas)
--- data/run.py
+++ data/run.py
@@ -7,12 +7,13 @@ from tqdm import tqdm
 logging.getLogger(__name__)
 
 def arg_parse():
-    arg = argparse.ArgumentParser()
-    arg.add_argument("--python_path", type=str, default="/root/anaconda3/envs/gpudrive/bin/python")
-    arg.add_argument("--store_path", type=str, default="/app/gpudrive_lab/data/storage.py")
-    arg.add_argument("--total_file_count", type=int, default=1000)
-    arg.add_argument("--learn_file_count", type=int, default=50)
-    return arg.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--python_path", type=str, default="/root/anaconda3/envs/gpudrive/bin/python")
+    parser.add_argument("--store_path", type=str, default="/app/data/storage.py")
+    parser.add_argument("--total_file_count", type=int, default=1000)
+    parser.add_argument("--learn_file_count", type=int, default=50)
+    parser.add_argument('--dataset', type=str, default='train', choices=['train', 'valid'],)
+    return parser.parse_args()
 
 if __name__ == "__main__":
     args = arg_parse()
@@ -24,7 +25,7 @@ if __name__ == "__main__":
     for i in tqdm(range(total_file_count // learn_file_count)):
         start_idx = learn_file_count * i
         
-        command = f"{args.python_path} {args.store_path} --start_idx {start_idx} --num_worlds {learn_file_count}"
+        command = f"{args.python_path} {args.store_path} --start_idx {start_idx} --num_worlds {learn_file_count} --dataset {args.dataset}"
         result = subprocess.run(command, shell=True)
         
         if result.returncode != 0:
