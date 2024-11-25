e83b8199da7fda8b6a1a2202895a3d1f810763c1
Kyungbeom Kim <69628911+KKGB@users.noreply.github.com>
2024-11-25 23:16:18 +0900

    Debug & Update storage (#35)
    * Fix: FeedForward input size
    
    * Fix: wandb id
    
    * Update: storage.py

--- algorithms/il/run_storage.py
+++ algorithms/il/run_storage.py
@@ -10,15 +10,15 @@ def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--python_path", type=str, default="/root/anaconda3/envs/gpudrive/bin/python")
     parser.add_argument("--store_path", type=str, default="algorithms/il/storage.py")
-    parser.add_argument("--total_file_count", type=int, default=200)
+    parser.add_argument("--total_file_count", type=int, default=1000)
     parser.add_argument("--learn_file_count", type=int, default=100)
-    parser.add_argument('--dataset', type=str, default='valid', choices=['train', 'valid'],)
-    
-    parser.add_argument('--num_worlds', type=int, default=200)
-    parser.add_argument('--save_index', type=int, default=0)
-    parser.add_argument('--save_path', type=str, default='/data')
     parser.add_argument('--dataset', type=str, default='train', choices=['train', 'valid'],)
-    parser.add_argument('--function', type=str, default='save_trajectory', choices=[
+    
+    parser.add_argument('--num_worlds', type=int, default=100)
+    parser.add_argument('--num_stack', type=int, default=5)
+    parser.add_argument('--save_index', type=int, default=0)
+    parser.add_argument('--save_path', type=str, default='/data/trajectories')
+    parser.add_argument('--function', type=str, default='save_obs_action_mean_std_mask_by_veh', choices=[
                                                                             'save_obs_action_mean_std_mask_by_veh',
                                                                             'save_trajectory',
                                                                             'save_trajectory_by_scenes',
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     for i in tqdm(range(total_file_count // learn_file_count)):
         save_index = learn_file_count * i
         
-        command = f"{args.python_path} {args.store_path} --num_worlds {learn_file_count} --save_index {save_index} --save_path {args.save_path} --dataset {args.dataset} --function {args.function}"
+        command = f"{args.python_path} {args.store_path} --num_worlds {learn_file_count} --num_stack {args.num_stack} --save_index {save_index} --save_path {args.save_path} --dataset {args.dataset} --function {args.function}"
         
         result = subprocess.run(command, shell=True)
         
