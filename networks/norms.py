eaee5b0367ae6a131835d03c1393bbc861f182d7
kkgb <kyungbeom8@naver.com>
2025-01-15 06:23:44 +0000

    Add: SetNorm

---
+++ networks/norms.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SetNorm(nn.LayerNorm):
+    def __init__(self, *args, feature_dim=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weights = nn.Parameter(torch.empty(feature_dim))
+        self.biases = nn.Parameter(torch.empty(feature_dim))
+        torch.nn.init.constant_(self.weights, 1.)
+        torch.nn.init.constant_(self.biases, 0.)
+
+    def forward(self, x, lengths=None):
+        # standardization
+        out = super().forward(x)
+        # transform params
+        out = F.linear(out, torch.diag_embed(self.weights), self.biases)
+        return out, lengths
