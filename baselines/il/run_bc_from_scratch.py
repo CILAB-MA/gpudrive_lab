4efb8dcbc39653346cb2eb36e74c06a3fc8df939
kevinjeon <kevinjeon119@gmail.com>
2025-02-02 01:23:55 +0900

    Add seeds and some optimizing codes

--- baselines/il/run_bc_from_scratch.py
+++ baselines/il/run_bc_from_scratch.py
@@ -49,6 +49,7 @@ def parse_args():
     
     # EXPERIMENT
     parser.add_argument('--exp-name', '-en', type=str, default='all_data')
+    parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--use-wandb', action='store_true')
     parser.add_argument('--sweep-id', type=str, default=None)
     parser.add_argument('--use-mask', action='store_true')
@@ -56,6 +57,16 @@ def parse_args():
     args = parser.parse_args()
     
     return args
+
+def set_seed(seed=42, deterministic=False):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False 
 
 def get_grad_norm(params, step=None):
     max_grad_norm = 0
@@ -99,7 +110,7 @@ def train():
     else:
         config = ExperimentConfig()
         config.__dict__.update(vars(args))
-    
+    set_seed(config.seed)    
     # Initialize model and optimizer
     bc_policy = MODELS[config.model_name](env_config, net_config, head_config, config.loss_name, config.num_stack,
                                           config.use_tom).to(config.device)
@@ -174,7 +185,8 @@ def train():
         ),
         batch_size=config.batch_size,
         shuffle=True,
-        num_workers=int(num_cpus / 2),
+        num_workers=num_cpus,
+        prefetch_factor=4,
         pin_memory=True
     )
     del train_expert_obs
@@ -196,7 +208,8 @@ def train():
         ),
         batch_size=config.batch_size,
         shuffle=False,
-        num_workers=int(num_cpus / 2),
+        num_workers=num_cpus,
+        prefetch_factor=4,
         pin_memory=True
     )
     del eval_expert_obs
@@ -397,7 +410,6 @@ def train():
 
 if __name__ == "__main__":
     args = parse_args()
-
     if args.use_wandb:
         with open("baselines/il/sweep.yaml") as f:
             exp_config = yaml.load(f, Loader=yaml.FullLoader)
