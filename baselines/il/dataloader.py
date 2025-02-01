66416bb0f42600a03dcd9b6d2cdceae481d59714
kevinjeon <kevinjeon119@gmail.com>
2025-02-02 01:21:18 +0900

    Change var names of dataloader.py and padding part

--- baselines/il/dataloader.py
+++ baselines/il/dataloader.py
@@ -13,9 +13,9 @@ class ExpertDataset(torch.utils.data.Dataset):
         self.actions = actions
         
         # masks
-        self.masks = 1 - masks
-        dead_masks_pad = np.ones((self.masks.shape[0], rollout_len - 1, *self.masks.shape[2:]), dtype=np.float32)
-        self.masks = np.concatenate([dead_masks_pad, self.masks], axis=1).astype('bool')
+        self.valid_masks = 1 - masks
+        dead_masks_pad = np.zeros((self.masks.shape[0], rollout_len - 1, *self.masks.shape[2:]), dtype=np.float32)
+        self.valid_masks = np.concatenate([dead_masks_pad, self.masks], axis=1).astype('bool')
         self.use_mask = True if self.masks is not None else False
 
         # partner_mask
@@ -25,7 +25,7 @@ class ExpertDataset(torch.utils.data.Dataset):
         
         # road_mask
         self.road_mask = road_mask
-        road_mask_pad = np.zeros((road_mask.shape[0], rollout_len - 1, *road_mask.shape[2:]), dtype=np.float32)
+        road_mask_pad = np.ones((road_mask.shape[0], rollout_len - 1, *road_mask.shape[2:]), dtype=np.float32)
         self.road_mask = np.concatenate([road_mask_pad, self.road_mask], axis=1).astype('bool')
         
         # other_info
@@ -34,7 +34,7 @@ class ExpertDataset(torch.utils.data.Dataset):
         self.other_info = np.concatenate([other_info_pad, self.other_info], axis=1)
         
         # ToM
-        self.aux_mask = np.where(partner_mask == 0, 1, 0).astype('bool')
+        self.aux_valid_mask = np.where(partner_mask == 0, 1, 0).astype('bool')
         if tom_time == 'only_pred':
             self.tom_timestep = pred_len
         elif tom_time == 'understand_pred':
@@ -44,16 +44,16 @@ class ExpertDataset(torch.utils.data.Dataset):
         self.rollout_len = rollout_len
         self.pred_len = pred_len
         self.valid_indices = self._compute_valid_indices()
-        self.full_var = ['obs', 'actions', 'masks', 'partner_mask', 'road_mask',
+        self.full_var = ['obs', 'actions', 'valid_masks', 'partner_mask', 'road_mask',
                          'other_info', 'aux_mask']
 
     def __len__(self):
         return len(self.valid_indices)
 
     def _compute_valid_indices(self):
-        N, T = self.masks.shape
+        N, T = self.valid_masks.shape
         valid_time = np.arange(T - (self.rollout_len + self.pred_len - 2))
-        valid_idx1, valid_idx2 = np.where(self.masks[:, valid_time + self.rollout_len + self.pred_len - 2] == 1)
+        valid_idx1, valid_idx2 = np.where(self.valid_masks[:, valid_time + self.rollout_len + self.pred_len - 2] == 1)
         valid_idx2 = valid_time[valid_idx2]
         return list(zip(valid_idx1, valid_idx2))
     
@@ -70,12 +70,12 @@ class ExpertDataset(torch.utils.data.Dataset):
                         data = self.__dict__[var_name][idx1, idx2:idx2 + self.pred_len] # idx 0 -> (0, 0:5) -> start with first timestep
                     elif var_name in ['other_info']:
                         data = self.__dict__[var_name][idx1, idx2:idx2 + self.tom_timestep] # idx 0 -> (0, 0:6) -> start with first timestep
-                    elif var_name == 'masks':
+                    elif var_name == 'valid_masks':
                         data = self.__dict__[var_name][idx1 ,idx2 + self.rollout_len + self.pred_len - 2] # idx 0 -> (0, 10 + 5 - 2) -> (0, 13) & padding = 9 -> end with last action timestep
                     else:
                         raise ValueError(f"Not in data {self.full_var}. Your input is {var_name}")
                     batch = batch + (data, )
-                    if var_name == 'masks':
+                    if var_name == 'valid_masks':
                         ego_mask_data = self.__dict__[var_name][idx1, idx2:idx2 + self.rollout_len]
                         batch = batch + (ego_mask_data, )
         else:
