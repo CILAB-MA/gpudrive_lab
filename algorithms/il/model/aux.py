c7535252fd0fbbb8b2cd9b427cd66b3e0d640e70
KKGB <kyungbeom8@naver.com>
2025-02-12 05:26:58 +0000

    Add: Aux Attention Score

--- algorithms/il/model/aux.py
+++ algorithms/il/model/aux.py
@@ -375,10 +375,15 @@ class LateFusionAttnAuxNet(CustomLateFusionNet):
         road_objects_max = road_objects_max.reshape(batch, -1)
         road_graph = road_graph.reshape(batch, -1)
         context = torch.cat((ego_attn, road_objects_max, road_graph), dim=1)
+        
+        ego_attn_score = objects_attn['ego_attn'].clone()
+        ego_attn_score = ego_attn_score.mean(dim=1)
+        ego_attn_score = ego_attn_score / ego_attn_score.sum(dim=-1, keepdim=True)
+        
         if self.use_tom == 'aux_head':
-            return context, mask_zero_ratio, other_objects, other_weights, max_indices_ro, max_indices_rg
+            return context, mask_zero_ratio, other_objects, other_weights, ego_attn_score, None
         else:
-            return context, mask_zero_ratio, None, None, max_indices_ro, max_indices_rg
+            return context, mask_zero_ratio, None, None, ego_attn_score, None
 
     def get_action(self, context, deterministic=False):
         """Get the action from the context."""
