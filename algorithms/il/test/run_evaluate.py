64c024b606963de6e4368087e3bec1282da8a827
kevinjeon <kevinjeon119@gmail.com>
2025-02-20 23:50:57 +0900

    Fix evaluate.py

--- algorithms/il/test/run_evaluate.py
+++ algorithms/il/test/run_evaluate.py
@@ -10,7 +10,8 @@ logging.getLogger(__name__)
 def arg_parse():
     parser = argparse.ArgumentParser()
     # MODEL
-    parser.add_argument('--model-path', '-mp', type=str, default='/data/model/eat100to1000')
+    parser.add_argument('--model-path', '-mp', type=str, default='/data/model/tom_03')
+    parser.add_argument('--video-path', '-vp', type=str, default='/data/video/tom_03')
     parser.add_argument("--total-world-count", type=int, default=20)
     parser.add_argument("--num-world", type=int, default=10)
     parser.add_argument('--start-idx', '-st', type=int, default=0)
@@ -29,8 +30,12 @@ if __name__ == "__main__":
         for dataset in ['train', 'valid']:
             for i in tqdm(range(args.start_idx, total_world_count // one_run_world_count)):
                 start_idx = i * one_run_world_count
+                if dataset == 'valid' and start_idx >= 200:
+                    break
                 print("model: ", model, "dataset: ", dataset, "idx:", start_idx)
-                arguments = f"-mc -mv --dataset {dataset} -mp {args.model_path} -mn {model} --num-world {one_run_world_count} --start-idx {start_idx}"
+                arguments = f"-mc --dataset {dataset} -mp {args.model_path} -vp {args.video_path} -mn {model} --num-world {one_run_world_count} --start-idx {start_idx}"
+                if i == 0:
+                    arguments += ' -mv'
                 command = f"CUDA_VISIBLE_DEVICES={args.gpu_id} /root/anaconda3/envs/gpudrive/bin/python algorithms/il/test/evaluate.py {arguments}"
                 
                 result = subprocess.run(command, shell=True)
