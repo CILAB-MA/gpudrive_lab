e251ab9beab63fed6a6aa9e941d42509bae5742b
KKGB <kyungbeom8@naver.com>
2025-03-14 08:21:19 +0000

    Update: storage (save obs, mean, std, valid_mask)

--- algorithms/il/run_storage.py
+++ algorithms/il/run_storage.py
@@ -9,11 +9,34 @@ logging.getLogger(__name__)
 def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--python_path", type=str, default="/root/anaconda3/envs/gpudrive/bin/python")
-    parser.add_argument("--store_path", type=str, default="/app/il/storage.py")
-    parser.add_argument("--total_file_count", type=int, default=1000)
-    parser.add_argument("--learn_file_count", type=int, default=50)
+    parser.add_argument("--store_path", type=str, default="algorithms/il/storage.py")
+    parser.add_argument("--total_file_count", type=int, default=10)
+    parser.add_argument("--learn_file_count", type=int, default=10)
     parser.add_argument('--dataset', type=str, default='train', choices=['train', 'valid'],)
+    
+    parser.add_argument('--num_stack', type=int, default=1)
+    parser.add_argument('--world_start_index', type=int, default=0)
+    parser.add_argument('--save_path', type=str, default='/data/RL/data/train_subset')
+
     return parser.parse_args()
+
+def get_least_used_gpu():
+    try:
+        result = subprocess.run(
+            ['nvidia-smi', '--query-gpu=memory.used', '--format=csv,noheader,nounits'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        if result.returncode != 0:
+            print("Error running nvidia-smi:", result.stderr)
+            return 0
+
+        memory_usage = [int(x) for x in result.stdout.strip().split('\n')]
+        return memory_usage.index(min(memory_usage))
+    except Exception as e:
+        print(f"Error detecting GPU: {e}")
+        return 0
 
 if __name__ == "__main__":
     args = arg_parse()
@@ -22,10 +45,12 @@ if __name__ == "__main__":
     
     print("start learning")
     
-    for i in tqdm(range(total_file_count // learn_file_count)):
-        start_idx = learn_file_count * i
+    for i in tqdm(range(args.world_start_index, total_file_count // learn_file_count)):
+        save_index = learn_file_count * i
+        gpu_index = get_least_used_gpu()
         
-        command = f"{args.python_path} {args.store_path} --start_idx {start_idx} --num_worlds {learn_file_count} --dataset {args.dataset}"
+        command = f"CUDA_VISIBLE_DEVICES={gpu_index} {args.python_path} {args.store_path} --num_worlds {learn_file_count} --num_stack {args.num_stack} --save_index {save_index} --save_path {args.save_path} --dataset {args.dataset}"
+        
         result = subprocess.run(command, shell=True)
         
         if result.returncode != 0:
--- algorithms/il/storage.py
+++ algorithms/il/storage.py
@@ -1,13 +1,14 @@
 import torch
 import numpy as np
+import os
 from tqdm import tqdm
 
 from pygpudrive.env.config import EnvConfig, SceneConfig, SelectionDiscipline
 
 
-def save_actions_by_veh(env):
+def save_mean_and_std_by_veh(env, save_path, save_index):
     """
-    Save the expert actions in the environment, distinguishing them by each vehicle.
+    Save the mean and std for 20 timesteps in the environment, distinguishing them by each vehicle.
     
     Args:
         env (GPUDriveTorchEnv): Initialized environment class.
@@ -19,281 +20,60 @@ def save_actions_by_veh(env):
     expert_actions, _, _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
     device = env.device
     
-    cont_agent_mask = env.cont_agent_mask.to(device)  # (num_worlds, num_agents)
-    alive_agent_indices = cont_agent_mask.nonzero(as_tuple=False)
-
-    alive_agent_num = env.cont_agent_mask.sum().item()
-    expert_actions_lst = [[None]*env.episode_len for _ in range(alive_agent_num)]
-    
-    
-    # Initialize dead agent mask
-    dead_agent_mask = ~env.cont_agent_mask.clone().to(device) # (num_worlds, num_agents)
-    
-    for time_step in tqdm(range(env.episode_len)):
-        # Step the environment with inferred expert actions
-        env.step_dynamics(expert_actions[:, :, time_step, :])
-        dones = env.get_dones().to(device)
-            
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
-        
-        for idx, (world_idx, agent_idx) in enumerate(alive_agent_indices):
-            if not dead_agent_mask[world_idx, agent_idx]:
-                expert_actions_lst[idx][time_step] = expert_actions[world_idx, agent_idx, time_step, :]
-        
-        if (dead_agent_mask == True).all():
-            break
-    
-    expert_actions_lst = [
-        [action for action in agent_actions if action is not None]
-        for agent_actions in expert_actions_lst
-    ]
-    
-    from MyTest.plot import plot_per_veh
-    plot_per_veh(expert_actions_lst, "/data/plots")
-    
-def save_obs_by_veh(env):
-    """
-    Save the expert obs in the environment, distinguishing them by each vehicle.
-    
-    Args:
-        env (GPUDriveTorchEnv): Initialized environment class.
-    """
-    obs = env.reset()
-    expert_actions, _, _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
-    device = env.device
-    
-    cont_agent_mask = env.cont_agent_mask.to(device)  # (num_worlds, num_agents)
+    cont_agent_mask = env.get_controlled_agents_mask().to(device)  # (num_worlds, num_agents)
     alive_agent_indices = cont_agent_mask.nonzero(as_tuple=False)
     alive_agent_num = env.cont_agent_mask.sum().item()
     print("alive_agent_num : ", alive_agent_num)
     
-    expert_obs = torch.zeros((alive_agent_num, env.episode_len, obs.shape[-1]), device=device)
-    expert_dead_mask_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device, dtype=torch.bool)
+    expert_obs_lst = torch.zeros((alive_agent_num, env.episode_len, obs.shape[-1]), device=device)
+    expert_actions_lst = torch.zeros((alive_agent_num, env.episode_len, expert_actions.shape[-1]), device=device)
+    expert_actions_mean_lst = torch.zeros((alive_agent_num, env.episode_len, 3), device=device)
+    expert_actions_std_lst = torch.zeros((alive_agent_num, env.episode_len, 3), device=device)
+    expert_valid_mask_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device, dtype=torch.bool)
     
     # Initialize dead agent mask
-    dead_agent_mask = ~env.cont_agent_mask.clone().to(device) # (num_worlds, num_agents)
+    valid_agent_mask = cont_agent_mask.clone().to(device) # (num_worlds, num_agents)
     
     for time_step in tqdm(range(env.episode_len)):
         for idx, (world_idx, agent_idx) in enumerate(alive_agent_indices):
-            if not dead_agent_mask[world_idx, agent_idx]:
-                expert_obs[idx][time_step] = obs[world_idx, agent_idx, :]
-            expert_dead_mask_lst[idx][time_step] = dead_agent_mask[world_idx, agent_idx]
-        
-        env.step_dynamics(expert_actions[:, :, time_step, :])
-        dones = env.get_dones().to(device)
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
-        
-        if (dead_agent_mask == True).all():
-            break
-        
-        obs = env.get_obs()
-    
-    expert_obs = expert_obs.to('cpu')
-    expert_dead_mask_lst = expert_dead_mask_lst.to('cpu')
-    np.savez_compressed("/data/train_trajectory_by_veh/obs_veh.npz", 
-                        obs=expert_obs,
-                        dead_mask=expert_dead_mask_lst)
-
-def save_mean_and_std_by_veh(env):
-    """
-    Save the mean and std for 10 timesteps in the environment, distinguishing them by each vehicle.
-    
-    Args:
-        env (GPUDriveTorchEnv): Initialized environment class.
-
-    Returns:
-        world_action_pairs: Expert actions for the controlled agents in each scene.
-    """
-    obs = env.reset()
-    expert_actions, _, _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
-    device = env.device
-    
-    cont_agent_mask = env.cont_agent_mask.to(device)  # (num_worlds, num_agents)
-    alive_agent_indices = cont_agent_mask.nonzero(as_tuple=False)
-    alive_agent_num = env.cont_agent_mask.sum().item()
-    print("alive_agent_num : ", alive_agent_num)
-    
-    expert_label_lst = torch.zeros((alive_agent_num, env.episode_len, 6), device=device)
-    expert_dead_mask_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device, dtype=torch.bool)
-    expert_actions_lst = torch.zeros((alive_agent_num, env.episode_len, 3), device=device)
-    
-    # Initialize dead agent mask
-    dead_agent_mask = ~env.cont_agent_mask.clone().to(device) # (num_worlds, num_agents)
-    
-    for time_step in tqdm(range(env.episode_len)):
-        for idx, (world_idx, agent_idx) in enumerate(alive_agent_indices):
-            if not dead_agent_mask[world_idx, agent_idx]:
+            if  valid_agent_mask[world_idx, agent_idx]:
+                expert_obs_lst[idx][time_step] = obs[world_idx, agent_idx]
                 expert_actions_lst[idx][time_step] = expert_actions[world_idx, agent_idx, time_step, :]
-            expert_dead_mask_lst[idx][time_step] = dead_agent_mask[world_idx, agent_idx]
+            expert_valid_mask_lst[idx][time_step] = valid_agent_mask[world_idx, agent_idx]
         
         env.step_dynamics(expert_actions[:, :, time_step, :])
+        obs = env.get_obs().to(device)
         dones = env.get_dones().to(device)
         
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
+        valid_agent_mask = torch.logical_and(valid_agent_mask, ~dones.bool())
         
-        if (dead_agent_mask == True).all():
+        if (valid_agent_mask == False).all():
             break
     
     # Future 10 step of actions mean and std
     for i in range(expert_actions_lst.shape[0]):
         for j in range(expert_actions_lst.shape[1]):
-            expert_label_lst[i][j][0:3] = expert_actions_lst[i][j:j+10].mean(dim=0)
-            expert_label_lst[i][j][3:6] = expert_actions_lst[i][j:j+10].std(dim=0)
-    
-    expert_label_lst = expert_label_lst.to('cpu')
-    expert_dead_mask_lst = expert_dead_mask_lst.to('cpu')
-    
-    np.savez_compressed("/data/train_trajectory_by_veh/label_veh.npz", 
-                        mean_std=expert_label_lst,
-                        dead_mask=expert_dead_mask_lst)
-
-def save_trajectory(env):
-    """
-    save the expert actions and observations in the environment (not distinguishing by each scene).
-    
-    Args:
-        env (GPUDriveTorchEnv): Initialized environment class.
-
-    Returns:
-        world_obs_pairs: Expert obs for the controlled agents in all scene.
-        world_action_pairs : Expert actions for the controlled agents in all scene.
-    """
-    obs = env.reset()
-    expert_actions, _, _ = env.get_expert_actions()
-    dead_agent_mask = ~env.cont_agent_mask.clone()
-
-    obs = obs.to('cpu')
-    expert_actions = expert_actions.to('cpu')
-    dead_agent_mask = dead_agent_mask.to('cpu')
-    
-    expert_obs_lst = []
-    expert_actions_lst = []
-    
-    # initial obs
-    expert_obs_lst.append(obs[~dead_agent_mask])
-    
-    for time_step in tqdm(range(env.episode_len)):
-        expert_actions_lst.append(
-            expert_actions[:,:,time_step,:][~dead_agent_mask]
-        )
-        
-        # Step the environment with inferred expert actions
-        env.step_dynamics(expert_actions[:, :, time_step, :], use_indices=False)
-        
-        # check dead agent
-        dones = env.get_dones().to('cpu')
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
-        if (dead_agent_mask == True).all():
-            break
-        
-        next_obs = env.get_obs().to('cpu')
-
-        expert_obs_lst.append(
-            next_obs[~dead_agent_mask]
-        )
-    
-    # world_obs_pairs = np.concatenate(expert_obs_lst, axis=0)
-    # world_action_pairs = np.concatenate(expert_actions_lst, axis=0)
-    
-
-    return expert_obs_lst, expert_actions_lst
-
-def save_actions_by_scenes(env):
-    """
-    Save the expert actions in the environment, distinguishing them by each scene.
-    
-    Args:
-        env (GPUDriveTorchEnv): Initialized environment class.
-
-    Returns:
-        world_action_pairs: Expert actions for the controlled agents in each scene.
-    """
-    obs = env.reset()
-    expert_actions, _, _ = env.get_expert_actions()
-
-    device = env.device
-    
-    expert_actions_lst = [[] for _ in range(expert_actions.shape[0])]
-    
-    # Initialize dead agent mask
-    dead_agent_mask = ~env.cont_agent_mask.clone().to(device)
-    
-    for time_step in tqdm(range(env.episode_len)):
-        # Step the environment with inferred expert actions
-        env.step_dynamics(expert_actions[:, :, time_step, :])
-        dones = env.get_dones().to(device)
-
-        for i in range(expert_actions.shape[0]):
-            expert_actions_lst[i].append(
-                expert_actions[i,:,time_step,:][~dead_agent_mask[i,:]]
-            )
+            actions_window = expert_actions_lst[i][j:j+20]
+            expert_actions_mean_lst[i][j] = actions_window.mean(dim=0)
+            if actions_window.shape[0] > 1:
+                expert_actions_std_lst[i][j] = actions_window.std(dim=0, unbiased=True)
+            else:
+                expert_actions_std_lst[i][j] = torch.zeros_like(actions_window[0])
             
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
-        
-        if (dead_agent_mask == True).all():
-            break
     
-    world_action_pairs = []
-    for x in expert_actions_lst:
-        world_action_pairs.append(torch.cat(x, dim=0))
+    expert_obs_lst = expert_obs_lst.to('cpu')
+    expert_actions_mean_lst = expert_actions_mean_lst.to('cpu')
+    expert_actions_std_lst = expert_actions_std_lst.to('cpu')
+    expert_valid_mask_lst = expert_valid_mask_lst.to('cpu')
     
-    return world_action_pairs
+    os.makedirs(save_path, exist_ok=True)
+    np.savez_compressed(f"{save_path}/trajectory_{save_index}.npz",
+                        obs=expert_obs_lst,
+                        mean=expert_actions_mean_lst,
+                        std=expert_actions_std_lst,
+                        valid_mask=expert_valid_mask_lst)
 
-def save_trajectory_by_scenes(env):
-    """
-    save the expert actions and observations in the environment (distinguishing by each scene).
-    
-    Args:
-        env (GPUDriveTorchEnv): Initialized environment class.
 
-    Returns:
-        world_obs_pairs: Expert obs for the controlled agents in all scene.
-        world_action_pairs : Expert actions for the controlled agents in all scene.
-    """
-    obs = env.reset()
-    expert_actions, _, _ = env.get_expert_actions()
-    dead_agent_mask = ~env.cont_agent_mask.clone()
-
-    obs = obs.to('cpu')
-    expert_actions = expert_actions.to('cpu')
-    dead_agent_mask = dead_agent_mask.to('cpu')
-    
-    expert_obs_lst = [[] for _ in range(expert_actions.shape[0])]
-    expert_actions_lst = [[] for _ in range(expert_actions.shape[0])]
-    
-    # initial obs
-    for i in range(obs.shape[0]):
-        expert_obs_lst[i].append(
-            obs[i,...][~dead_agent_mask[i,...]]
-        )
-    
-    
-    for time_step in tqdm(range(env.episode_len)):
-        for i in range(expert_actions.shape[0]):
-            expert_actions_lst[i].append(
-                expert_actions[i,:,time_step,:][~dead_agent_mask[i,:]]
-            )
-        
-        # Step the environment with inferred expert actions
-        env.step_dynamics(expert_actions[:, :, time_step, :], use_indices=False)
-        
-        # check dead agent
-        dones = env.get_dones().to('cpu')
-        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
-        if (dead_agent_mask == True).all():
-            break
-        
-        next_obs = env.get_obs().to('cpu')
-        for i in range(next_obs.shape[0]):
-            expert_obs_lst[i].append(
-                next_obs[i,...][~dead_agent_mask[i,...]]
-            )
-    
-    world_obs_pairs = [np.concatenate(scene_data, axis=0) for scene_data in expert_obs_lst]
-    world_action_pairs = [np.concatenate(scene_data, axis=0) for scene_data in expert_actions_lst]
-    
-
-    return world_obs_pairs, world_action_pairs
 
 
 if __name__ == "__main__":
@@ -301,25 +81,29 @@ if __name__ == "__main__":
     from pygpudrive.registration import make
     from pygpudrive.env.config import DynamicsModel, ActionSpace
     parser = argparse.ArgumentParser('Select the dynamics model that you use')
-    parser.add_argument('--dynamics-model', '-dm', type=str, default='delta_local', choices=['delta_local', 'bicycle', 'classic'],)
-    parser.add_argument('--device', '-d', type=str, default='cuda', choices=['cpu', 'cuda'],)
-    parser.add_argument('--num_worlds', type=int, default=50)
-    parser.add_argument('--start_idx', type=int, default=0)
-    parser.add_argument('--save_path', type=str, default='/data/train_trajectory_by_veh')
+    parser.add_argument('--num_worlds', type=int, default=5)
+    parser.add_argument('--num_stack', type=int, default=1)
+    parser.add_argument('--save_path', type=str, default='/data/RL/data')
+    parser.add_argument('--save_index', type=int, default=0)
     parser.add_argument('--dataset', type=str, default='train', choices=['train', 'valid'],)
     args = parser.parse_args()
 
     torch.set_printoptions(precision=3, sci_mode=False)
-    NUM_WORLDS = args.num_worlds
-    MAX_NUM_OBJECTS = 128
+    
+    print()
+    print("num_worlds : ", args.num_worlds)
+    print("num_stack : ", args.num_stack)
+    print("save_path : ", args.save_path)
+    print("save_index : ", args.save_index)
+    print("dataset : ", args.dataset)
 
     # Initialize configurations
     scene_config = SceneConfig(f"/data/formatted_json_v2_no_tl_{args.dataset}/",
-                               NUM_WORLDS,
-                               start_idx=args.start_idx,
+                               num_scenes=args.num_worlds,
+                               start_idx=args.save_index,
                                discipline=SelectionDiscipline.RANGE_N)
     env_config = EnvConfig(
-        dynamics_model=args.dynamics_model,
+        dynamics_model='delta_local',
         steer_actions=torch.round(torch.tensor([-np.inf, np.inf]), decimals=3),
         accel_actions=torch.round(torch.tensor([-np.inf, np.inf]), decimals=3),
         dx=torch.round(torch.tensor([-np.inf, np.inf]), decimals=3),
@@ -331,14 +115,14 @@ if __name__ == "__main__":
     kwargs={
         "config": env_config,
         "scene_config": scene_config,
-        "max_cont_agents": MAX_NUM_OBJECTS,
-        "device": args.device,
-        "num_stack": 1
+        "max_cont_agents": 128,
+        "device": "cuda",
+        "num_stack": args.num_stack
     }
     
     env = make(dynamics_id=DynamicsModel.DELTA_LOCAL, action_id=ActionSpace.CONTINUOUS, kwargs=kwargs)
 
-    save_mean_and_std_by_veh(env)
+    save_mean_and_std_by_veh(env, args.save_path, args.save_index)
 
     env.close()
     del env
