f3d975adae9eaab6b082a009ff6daa90b1acd00a
Hyeonchang Jeon <kevinjeon119@gmail.com>
2025-04-11 13:39:56 +0900

    Update extract_sweep & Normalize goal metric (#127)
    * Update sweep files for seed
    
    * Normalize goal reached time with expert time

--- algorithms/il/analyze/linear_probing/extract_sweep.py
+++ algorithms/il/analyze/linear_probing/extract_sweep.py
@@ -5,7 +5,7 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser('Select the dynamics model that you use')
     parser.add_argument('--save-name', '-sn', type=str, default='scene500')
-    parser.add_argument('--linear-probing', type=str, default="partner")   
+    parser.add_argument('--linear-probing', '-lp', type=str, default="partner")   
     parser.add_argument("--sweep-ids", '-s', nargs="+", type=str)
     args = parser.parse_args()
     
@@ -26,24 +26,38 @@ if __name__ == '__main__':
         sweep = api.sweep(f"{ENTITY}/{PROJECT}/{sweep_id}")
         
         for run in sweep.runs:
-            summary = run.summary._json_dict
             config = run.config
             name = run.name
 
-            summary.update(config)
-            summary["run_name"] = name
-            summary["sweep_id"] = sweep_id
-            eval_summary = {k:v for k, v in summary.items() if k.startswith("eval/")}
+            # step=20일 때의 로그 가져오기
+            history = run.history()  # 충분히 커야 함
+            if '_step' not in history.keys():
+                continue
+            step20 = history[history['_step'] == 20]
+
+            if step20.empty:
+                continue  # 해당 step 없으면 스킵
+
+            row = step20.iloc[0].to_dict()
+            eval_summary = {k: v for k, v in row.items() if k.startswith("eval/")}
+
+            # 나머지 정보도 추가
             if args.linear_probing == 'ego':
                 future_step = "ego_future_step"
             elif args.linear_probing == 'partner':
                 future_step = "aux_future_step"
-            eval_summary[future_step] = summary[future_step]
-            eval_summary["experiment"] = summary['model']
+
+            # 여긴 여전히 summary에서 가져와야 함 (step과 무관하니까)
+            summary = run.summary._json_dict
+            eval_summary[future_step] = config.get(future_step)
+            eval_summary["experiment"] = config.get("model")
+            eval_summary["seed"] = config.get("seed")
+
             all_runs.append(eval_summary)
 
-    df = pd.DataFrame(all_runs)
 
+    df = pd.DataFrame(all_runs)
+    print(df)
     csv_filename = f"/data/linear_probingv2/{args.linear_probing}_{args.save_name}.csv"
     os.makedirs(f"/data/linear_probingv2", exist_ok=True)
     df.to_csv(csv_filename, index=False)
