6c7af3ff016f6c2af4054c4c2ad364acc2c49777
KevinJeon <kevinjeon119@gmail.com>
2025-06-17 11:35:07 +0000

    Debug simulation

--- gpudrive/integrations/reasoning/match_qa.py
+++ gpudrive/integrations/reasoning/match_qa.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('Simulation experiment')
     parser.add_argument("--data_dir", "-dd", type=str, default="validation", help="training (80000) / testing (10000)")
     parser.add_argument('--make-video', '-mv', action='store_true')
-    parser.add_argument("--video-dir", "-vd", type=str, default="/data/full_version/expert_video/validation_log")
     parser.add_argument("--total-scene-size", "-tss", type=int, default=10000)
     parser.add_argument("--scene-batch-size", "-sbs", type=int, default=100)
     parser.add_argument("--max-cont-agents", "-m", type=int, default=128)
@@ -23,7 +22,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     DATA_DIR = os.path.join("/data/full_version/data", args.data_dir)
-    VIDEO_DIR = args.video_dir
     TOTAL_NUM_WORLDS = args.total_scene_size
     NUM_WORLDS = args.scene_batch_size
     json_folder = args.data_dir + '_interactive' if args.data_dir == 'validation' else args.data_dir
@@ -50,13 +48,14 @@ if __name__ == "__main__":
     )
     num_iter = int(TOTAL_NUM_WORLDS // NUM_WORLDS)
     for idx in tqdm(range(num_iter)):
-        with open(f"/data/full_version/processed/reasoning/validation/womd_reasoning_{100 * idx}.json", "r") as f:
+        with open(f"/data/full_version/processed/reasoning/{args.data_dir}/womd_reasoning_{100 * idx}.json", "r") as f:
             jd = json.load(f)
+        obs = env.reset()
         jd_scenario_ids = list(jd.keys())
         scenario_ids = env.get_scenario_ids()
         ego_ids = env.get_ego_ids()
         for scene_id, data in jd.items():
-            print(scene_id, data)
+            print(ego_ids[data['batch_idx']].int()[:10], data['ego'], data['rel_id'], data['sid'], scenario_ids[data['batch_idx']])
         if idx != num_iter - 1:
             env.swap_data_batch()
     env.close()
