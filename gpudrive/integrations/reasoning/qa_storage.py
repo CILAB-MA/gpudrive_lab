fdb36a98acbf9f1f17b14c5b3af0f783b5301b89
KevinJeon <kevinjeon119@gmail.com>
2025-07-17 04:58:37 +0000

    Change storage for getting whole params

--- gpudrive/integrations/reasoning/qa_storage.py
+++ gpudrive/integrations/reasoning/qa_storage.py
@@ -11,47 +11,19 @@ from gpudrive.env.dataset import SceneDataLoader
 from gpudrive.visualize.utils import img_from_fig
 from tqdm import tqdm
 
-from transformers import AutoTokenizer, AutoModel
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 import torch
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
 
-@torch.no_grad()
-def compute_sentence_embeddings(questions, model_name='all-MiniLM-L6-v2', device='cuda', name='env'):
-    model = SentenceTransformer('all-MiniLM-L6-v2') 
-    model.eval()
-    embeddings  = model.encode(questions, convert_to_tensor=True)  
-    return embeddings.cpu().numpy()  # (N, hidden_dim)
-
-def analyze_semantic_distribution_hf(questions, n_clusters=10, save_path="tsne_plot.png"):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    embeddings = compute_sentence_embeddings(questions, device=device)
-
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42).fit(embeddings)
-    labels = kmeans.labels_
-    sil_score = silhouette_score(embeddings, labels)
-    print(f"Silhouette Score: {sil_score:.4f}")
-
-    tsne = TSNE(n_components=2, perplexity=30, random_state=42)
-    reduced = tsne.fit_transform(embeddings)
-
-    plt.figure(figsize=(8, 6))
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(x=reduced[:, 0], y=reduced[:, 1], hue=labels, palette="tab10", s=30)
-    plt.tight_layout()
-    plt.savefig(save_path)
-    print(f"Saved t-SNE plot to: {os.path.abspath(save_path)}")
-    for i in range(10):
-        print(f"\n[Cluster {i}]")
-        for q in np.array(questions)[labels == i][:5]:
-            print(" -", q)
-    return embeddings
+# @torch.no_grad()
+# def compute_sentence_embeddings(questions, model_name='all-MiniLM-L6-v2', device='cuda', name='env'):
+#     model = SentenceTransformer('all-MiniLM-L6-v2') 
+#     model.eval()
+#     embeddings  = model.encode(questions, convert_to_tensor=True)  
+#     return embeddings.cpu().numpy()  # (N, hidden_dim)
 
 def save_qa_trajectory(env, model, jd, save_path, save_index=0):
     """
@@ -62,39 +34,39 @@ def save_qa_trajectory(env, model, jd, save_path, save_index=0):
     """
     qa_types = ["env", "ego", "sur", "int"]
     valid_agent = len(jd)
-    qa_timesteps = 20
-    expert_env_q_lst = np.zeros((valid_agent, 20, 384))
-    expert_ego_q_lst = np.zeros((valid_agent, 20, 384))
-    expert_sur_q_lst = np.zeros((valid_agent, 120, 384))
-    expert_int_q_lst = np.zeros((valid_agent, 30, 384))
+    qa_timesteps = env.episode_len
+    # expert_env_q_lst = np.zeros((valid_agent, 20, 384))
+    # expert_ego_q_lst = np.zeros((valid_agent, 20, 384))
+    # expert_sur_q_lst = np.zeros((valid_agent, 120, 384))
+    # expert_int_q_lst = np.zeros((valid_agent, 30, 384))
 
-    expert_env_a_lst = np.zeros((valid_agent, 20, 384))
-    expert_ego_a_lst = np.zeros((valid_agent, 20, 384))
-    expert_sur_a_lst = np.zeros((valid_agent, 120, 384))
-    expert_int_a_lst = np.zeros((valid_agent, 30, 384))
-    q_npy = [expert_env_q_lst, expert_ego_q_lst, expert_sur_q_lst, expert_int_q_lst]
-    a_npy = [expert_env_a_lst, expert_ego_a_lst, expert_sur_a_lst, expert_int_a_lst]
+    # expert_env_a_lst = np.zeros((valid_agent, 20, 384))
+    # expert_ego_a_lst = np.zeros((valid_agent, 20, 384))
+    # expert_sur_a_lst = np.zeros((valid_agent, 120, 384))
+    # expert_int_a_lst = np.zeros((valid_agent, 30, 384))
+    # q_npy = [expert_env_q_lst, expert_ego_q_lst, expert_sur_q_lst, expert_int_q_lst]
+    # a_npy = [expert_env_a_lst, expert_ego_a_lst, expert_sur_a_lst, expert_int_a_lst]
 
-    expert_env_mask_lst = np.ones((valid_agent, 20), dtype=bool)
-    expert_ego_mask_lst = np.ones((valid_agent, 20), dtype=bool)
-    expert_sur_mask_lst = np.ones((valid_agent, 120), dtype=bool)
-    expert_int_mask_lst = np.ones((valid_agent, 30), dtype=bool)
-    qa_mask_npy = [expert_env_mask_lst, expert_ego_mask_lst, expert_sur_mask_lst, expert_int_mask_lst]
-    for i, data in enumerate(jd.values()):
-        for qa, qa_type in enumerate(qa_types):
-            qas = data[f"{qa_type}_qa"]
-            if qas:
-                qs, ans = zip(*qas) 
-            else:
-                continue
-            q_embeddings  = model.encode(qs, convert_to_tensor=True)  
-            q_embeddings =  q_embeddings.cpu().numpy()  #
-            a_embeddings  = model.encode(ans, convert_to_tensor=True)  
-            a_embeddings =  a_embeddings.cpu().numpy()  #
-            num = min(len(q_embeddings), qa_timesteps)
-            q_npy[qa][i, :num] = q_embeddings[:num]
-            a_npy[qa][i, :num] = a_embeddings[:num]
-            qa_mask_npy[qa][i, :num] = False
+    # expert_env_mask_lst = np.ones((valid_agent, 20), dtype=bool)
+    # expert_ego_mask_lst = np.ones((valid_agent, 20), dtype=bool)
+    # expert_sur_mask_lst = np.ones((valid_agent, 120), dtype=bool)
+    # expert_int_mask_lst = np.ones((valid_agent, 30), dtype=bool)
+    # qa_mask_npy = [expert_env_mask_lst, expert_ego_mask_lst, expert_sur_mask_lst, expert_int_mask_lst]
+    # for i, data in enumerate(jd.values()):
+    #     for qa, qa_type in enumerate(qa_types):
+    #         qas = data[f"{qa_type}_qa"]
+    #         if qas:
+    #             qs, ans = zip(*qas) 
+    #         else:
+    #             continue
+    #         q_embeddings  = model.encode(qs, convert_to_tensor=True)  
+    #         q_embeddings =  q_embeddings.cpu().numpy()  #
+    #         a_embeddings  = model.encode(ans, convert_to_tensor=True)  
+    #         a_embeddings =  a_embeddings.cpu().numpy()  #
+    #         num = min(len(q_embeddings), qa_timesteps)
+    #         q_npy[qa][i, :num] = q_embeddings[:num]
+    #         a_npy[qa][i, :num] = a_embeddings[:num]
+    #         qa_mask_npy[qa][i, :num] = False
     obs = env.reset()
     expert_actions, _, _, _ , _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
     road_mask = env.get_road_mask()
@@ -125,7 +97,7 @@ def save_qa_trajectory(env, model, jd, save_path, save_index=0):
 
     for time_step in tqdm(range(env.episode_len)):
         for idx, (world_idx, agent_idx) in enumerate(zip(scene_idx, qa_ego_idx)):
-            if (not dead_agent_mask[world_idx, agent_idx]) and (time_step < 30) and (time_step >= 10):
+            if not dead_agent_mask[world_idx, agent_idx]:
                 expert_trajectory_lst[idx][time_step - 10] = obs[world_idx, agent_idx]
                 expert_actions_lst[idx][time_step - 10] = expert_actions[world_idx, agent_idx, time_step]
                 expert_partner_mask_lst[idx][time_step - 10] = partner_mask[world_idx, agent_idx]
@@ -170,46 +142,46 @@ def save_qa_trajectory(env, model, jd, save_path, save_index=0):
     expert_global_pos_lst = expert_global_pos_lst[~collision].to('cpu')
     expert_global_rot_lst = expert_global_rot_lst[~collision].to('cpu')
 
-    expert_env_q_lst = q_npy[0][~collision.cpu().numpy()]
-    expert_ego_q_lst = q_npy[1][~collision.cpu().numpy()]
-    expert_sur_q_lst = q_npy[2][~collision.cpu().numpy()]
-    expert_int_q_lst = q_npy[3][~collision.cpu().numpy()]
+    # expert_env_q_lst = q_npy[0][~collision.cpu().numpy()]
+    # expert_ego_q_lst = q_npy[1][~collision.cpu().numpy()]
+    # expert_sur_q_lst = q_npy[2][~collision.cpu().numpy()]
+    # expert_int_q_lst = q_npy[3][~collision.cpu().numpy()]
 
-    expert_env_a_lst = a_npy[0][~collision.cpu().numpy()]
-    expert_ego_a_lst = a_npy[1][~collision.cpu().numpy()]
-    expert_sur_a_lst = a_npy[2][~collision.cpu().numpy()]
-    expert_int_a_lst = a_npy[3][~collision.cpu().numpy()]
+    # expert_env_a_lst = a_npy[0][~collision.cpu().numpy()]
+    # expert_ego_a_lst = a_npy[1][~collision.cpu().numpy()]
+    # expert_sur_a_lst = a_npy[2][~collision.cpu().numpy()]
+    # expert_int_a_lst = a_npy[3][~collision.cpu().numpy()]
 
-    expert_env_mask_lst = qa_mask_npy[0][~collision.cpu().numpy()]
-    expert_ego_mask_lst = qa_mask_npy[1][~collision.cpu().numpy()]
-    expert_sur_mask_lst = qa_mask_npy[2][~collision.cpu().numpy()]
-    expert_int_mask_lst = qa_mask_npy[3][~collision.cpu().numpy()]
+    # expert_env_mask_lst = qa_mask_npy[0][~collision.cpu().numpy()]
+    # expert_ego_mask_lst = qa_mask_npy[1][~collision.cpu().numpy()]
+    # expert_sur_mask_lst = qa_mask_npy[2][~collision.cpu().numpy()]
+    # expert_int_mask_lst = qa_mask_npy[3][~collision.cpu().numpy()]
     os.makedirs(save_path, exist_ok=True)
     os.makedirs(save_path + '/global', exist_ok=True)
     os.makedirs(save_path + '/reasoning', exist_ok=True)
-    np.savez_compressed(f"{save_path}/trajectory_{save_index}.npz", 
-                        obs=expert_trajectory_lst,
-                        actions=expert_actions_lst,
-                        dead_mask=expert_dead_mask_lst,
-                        partner_mask=expert_partner_mask_lst,
-                        road_mask=expert_road_mask_lst)
-    np.savez_compressed(f"{save_path}/reasoning/reasoning_trajectory_{save_index}.npz", 
-                        env_q=expert_env_q_lst,
-                        ego_q=expert_ego_q_lst,
-                        sur_q=expert_sur_q_lst,
-                        int_q=expert_int_q_lst,
-                        env_a=expert_env_a_lst,
-                        ego_a=expert_ego_a_lst,
-                        sur_a=expert_sur_a_lst,
-                        int_a=expert_int_a_lst,
-                        env_mask=expert_env_mask_lst,
-                        ego_mask=expert_ego_mask_lst,
-                        sur_mask=expert_sur_mask_lst,
-                        int_mask=expert_int_mask_lst,
-                        )
-    np.savez_compressed(f"{save_path}/global/global_trajectory_{save_index}.npz", 
-                        ego_global_pos=expert_global_pos_lst,
-                        ego_global_rot=expert_global_rot_lst)
+    # np.savez_compressed(f"{save_path}/trajectory_{save_index}.npz", 
+    #                     obs=expert_trajectory_lst,
+    #                     actions=expert_actions_lst,
+    #                     dead_mask=expert_dead_mask_lst,
+    #                     partner_mask=expert_partner_mask_lst,
+    #                     road_mask=expert_road_mask_lst)
+    # np.savez_compressed(f"{save_path}/reasoning/reasoning_trajectory_{save_index}.npz", 
+    #                     env_q=expert_env_q_lst,
+    #                     ego_q=expert_ego_q_lst,
+    #                     sur_q=expert_sur_q_lst,
+    #                     int_q=expert_int_q_lst,
+    #                     env_a=expert_env_a_lst,
+    #                     ego_a=expert_ego_a_lst,
+    #                     sur_a=expert_sur_a_lst,
+    #                     int_a=expert_int_a_lst,
+    #                     env_mask=expert_env_mask_lst,
+    #                     ego_mask=expert_ego_mask_lst,
+    #                     sur_mask=expert_sur_mask_lst,
+    #                     int_mask=expert_int_mask_lst,
+    #                     )
+    # np.savez_compressed(f"{save_path}/global/global_trajectory_{save_index}.npz", 
+    #                     ego_global_pos=expert_global_pos_lst,
+    #                     ego_global_rot=expert_global_rot_lst)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Simulation experiment')
@@ -252,8 +224,9 @@ if __name__ == "__main__":
         device="cuda",
         action_type="continuous",
     )
-    model = SentenceTransformer('all-MiniLM-L6-v2') 
-    model.eval()
+    # model = SentenceTransformer('all-MiniLM-L6-v2') 
+    # model.eval()
+    model = None
     num_iter = int(TOTAL_NUM_WORLDS // NUM_WORLDS)
     print('Launch Env')
     num_iter = int(args.total_scene_size // args.scene_batch_size)
