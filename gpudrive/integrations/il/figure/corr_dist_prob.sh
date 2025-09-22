9dd14a6ebb4153a3b8cc858b2cd6a92145f291d7
KevinJeon <kevinjeon119@gmail.com>
2025-09-22 08:51:21 +0000

    Update visualization

--- gpudrive/integrations/il/figure/corr_dist_prob.sh
+++ gpudrive/integrations/il/figure/corr_dist_prob.sh
@@ -3,11 +3,12 @@
 # Bash Script for Running WandB Agent with GPU ID, Sweep ID, and WandB API Key
 # Usage: ./partner_ratio.sh 1 2 3 4
 
-MODEL_PATH=("exp_100" "exp_500" "exp_5000" "exp_10000_v2")
+MODEL_PATH=("exp_100" "exp_80000_subset_aix")
+NUM_SCENE=(100 80000)
 # 반복문 실행
 for i in "${!MODEL_PATH[@]}"; do
   MP="${MODEL_PATH[$i]}"
-  MN="${MODEL_NAME[$i]}"
+  NS="${NUM_SCENE[$i]}"
   CUDA_VISIBLE_DEVICES=1 python baselines/il/test/evaluate_lp.py \
-    --model-path "$MP"
+    --model-path "$MP" --num-scene $NS
 done
