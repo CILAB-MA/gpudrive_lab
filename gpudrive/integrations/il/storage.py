f98b6edefa0c4541d42954a60b62197361a0bf11
KKGB <kyungbeom8@naver.com>
2025-09-29 13:28:19 +0900

    Update: storage & il train dataloader

--- gpudrive/integrations/il/storage.py
+++ gpudrive/integrations/il/storage.py
@@ -18,7 +18,6 @@ def save_trajectory(env, save_path, save_index=0):
     expert_actions, _, _, _ , _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
     road_mask = env.get_road_mask()
     partner_mask = env.get_partner_mask()
-    # partner_id = env.get_partner_id().unsqueeze(-1)
     device = env.device
     
     cont_agent_mask = env.cont_agent_mask.to(device)  # (num_worlds, num_agents)
@@ -26,16 +25,14 @@ def save_trajectory(env, save_path, save_index=0):
     alive_agent_num = env.cont_agent_mask.sum().item()
     print("alive_agent_num : ", alive_agent_num)
     
-    expert_trajectory_lst = torch.zeros((alive_agent_num, env.episode_len, obs.shape[-1]), device=device)
+    expert_trajectory_lst = torch.zeros((alive_agent_num, env.episode_len + 4, obs.shape[-1]), device=device)
     expert_actions_lst = torch.zeros((alive_agent_num, env.episode_len, 3), device=device)
-    expert_dead_mask_lst = torch.ones((alive_agent_num, env.episode_len), device=device, dtype=torch.bool)
-    expert_partner_mask_lst = torch.full((alive_agent_num, env.episode_len, 127), 2, device=device, dtype=torch.long)
-    expert_road_mask_lst = torch.ones((alive_agent_num, env.episode_len, 200), device=device, dtype=torch.bool)
+    expert_valid_mask_lst = torch.zeros((alive_agent_num, env.episode_len + 4), device=device, dtype=torch.bool)
+    expert_partner_mask_lst = torch.ones((alive_agent_num, env.episode_len + 4, 127), device=device, dtype=torch.bool)
+    expert_road_mask_lst = torch.ones((alive_agent_num, env.episode_len + 4, 200), device=device, dtype=torch.bool)
     expert_global_pos_lst = torch.zeros((alive_agent_num, env.episode_len, 2), device=device) # global pos (2)
     expert_global_rot_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device) # global actions (1)
-    expert_partner_id_lst = torch.zeros((alive_agent_num, env.episode_len, 127), device=device) # global actions (1)
-    expert_ego_id_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device) # global actions (1)
-    expert_scene_id_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device) # global actions (1)
+
     # Initialize dead agent mask
     agent_info = (
             env.sim.absolute_self_observation_tensor()
@@ -50,16 +47,20 @@ def save_trajectory(env, save_path, save_index=0):
     for time_step in tqdm(range(env.episode_len)):
         for idx, (world_idx, agent_idx) in enumerate(alive_agent_indices):
             if not dead_agent_mask[world_idx, agent_idx]:
-                expert_trajectory_lst[idx][time_step] = obs[world_idx, agent_idx]
+                expert_trajectory_lst[idx][time_step + 4] = obs[world_idx, agent_idx]
                 expert_actions_lst[idx][time_step] = expert_actions[world_idx, agent_idx, time_step]
-                expert_partner_mask_lst[idx][time_step] = partner_mask[world_idx, agent_idx]
-                expert_road_mask_lst[idx][time_step] = road_mask[world_idx, agent_idx]
+                expert_partner_mask_lst[idx][time_step + 4] = partner_mask[world_idx, agent_idx] == 2
+                expert_road_mask_lst[idx][time_step + 4] = road_mask[world_idx, agent_idx]
                 expert_global_pos_lst[idx, time_step] = agent_info[world_idx, agent_idx, 0:2]
                 expert_global_rot_lst[idx, time_step] = agent_info[world_idx, agent_idx, 7:8]
-                expert_partner_id_lst[idx, time_step] = env.partner_ids[world_idx, agent_idx].clone()
-                expert_ego_id_lst[idx, time_step] = agent_info[world_idx, agent_idx, -1]
-                expert_scene_id_lst[idx, time_step] = world_idx
-            expert_dead_mask_lst[idx][time_step] = dead_agent_mask[world_idx, agent_idx]
+                # expert_partner_id_lst[idx, time_step] = env.partner_ids[world_idx, agent_idx].clone()
+                # expert_ego_id_lst[idx, time_step] = agent_info[world_idx, agent_idx, -1]
+                # expert_scene_id_lst[idx, time_step] = world_idx
+            expert_valid_mask_lst[idx][time_step + 4] = (~dead_agent_mask[world_idx, agent_idx]) & ~(
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, 1]) >  0.5) | 
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, 0]) >  5) | 
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, -1]) > 0.2)
+                )
 
         
         # env.step() -> gather next obs
@@ -96,47 +97,42 @@ def save_trajectory(env, save_path, save_index=0):
     
     expert_trajectory_lst = expert_trajectory_lst[goal_mask].to('cpu')
     expert_actions_lst = expert_actions_lst[goal_mask].to('cpu')
-    expert_dead_mask_lst = expert_dead_mask_lst[goal_mask].to('cpu')
+    expert_valid_mask_lst = expert_valid_mask_lst[goal_mask].to('cpu')
     expert_partner_mask_lst = expert_partner_mask_lst[goal_mask].to('cpu')
     expert_road_mask_lst = expert_road_mask_lst[goal_mask].to('cpu')
     # global pos
     expert_global_pos_lst = expert_global_pos_lst[goal_mask].to('cpu')
     expert_global_rot_lst = expert_global_rot_lst[goal_mask].to('cpu')
-    expert_partner_id_lst = expert_partner_id_lst[goal_mask].to('cpu')
-    expert_ego_id_lst = expert_ego_id_lst[goal_mask].to('cpu')
-    expert_scene_id_lst = expert_scene_id_lst[goal_mask].to('cpu')
+
     os.makedirs(save_path, exist_ok=True)
     os.makedirs(save_path + '/global', exist_ok=True)
-    os.makedirs(save_path + '/id', exist_ok=True)
     np.savez_compressed(f"{save_path}/trajectory_{save_index}.npz", 
                         obs=expert_trajectory_lst,
                         actions=expert_actions_lst,
-                        dead_mask=expert_dead_mask_lst,
+                        valid_mask=expert_valid_mask_lst,
                         partner_mask=expert_partner_mask_lst,
                         road_mask=expert_road_mask_lst)
     np.savez_compressed(f"{save_path}/global/global_trajectory_{save_index}.npz", 
                         ego_global_pos=expert_global_pos_lst,
                         ego_global_rot=expert_global_rot_lst)
-    np.savez_compressed(f"{save_path}/id/id_trajectory_{save_index}.npz", 
-                        ego_id=expert_ego_id_lst,
-                        scene_id=expert_scene_id_lst,
-                        partner_id=expert_partner_id_lst)
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_stack', type=int, default=1)
-    parser.add_argument('--save_path', type=str, default='/data/full_version/processed/')
-    parser.add_argument('--dataset', type=str, default='training', choices=['training', 'validation', 'testing'],)
+    parser.add_argument('--save_path', type=str, default='/data/full_version/processed/final2')
+    parser.add_argument('--dataset', type=str, default='validation', choices=['training', 'validation', 'testing'],)
     parser.add_argument('--function', type=str, default='save_trajectory', 
                         choices=[
                             'save_trajectory'])
-    parser.add_argument('--dataset-size', type=int, default=80000) # total_world
+    parser.add_argument('--dataset-size', type=int, default=10000) # total_world
     parser.add_argument('--batch-size', type=int, default=100) # num_world
     parser.add_argument('--start-idx', type=int, default=None, help="start scene number of dataset")
     args = parser.parse_args()
 
     torch.set_printoptions(precision=3, sci_mode=False)
-    save_path = os.path.join(args.save_path, f'{args.dataset}_only_goal')
+    save_path = os.path.join(args.save_path, f'{args.dataset}_subset_v6')
     print()
     print("num_stack : ", args.num_stack)
     print("save_path : ", save_path)
