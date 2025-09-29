557a2b0a9b1fae057525c694753b6fd5baed1daf
KKGB <kyungbeom8@naver.com>
2025-09-29 11:39:44 +0000

    Add: save_trajectory_npy

--- gpudrive/integrations/il/storage.py
+++ gpudrive/integrations/il/storage.py
@@ -116,17 +116,130 @@ def save_trajectory(env, save_path, save_index=0):
                         ego_global_pos=expert_global_pos_lst,
                         ego_global_rot=expert_global_rot_lst)
 
+def save_trajectory_npy(env, save_path, save_index=0):
+    """
+    Save the trajectory, partner_mask and road_mask in the environment, distinguishing them by each scene and agent.
+    
+    Args:
+        env (GPUDriveTorchEnv): Initialized environment class.
+    """
+    obs = env.reset()
+    expert_actions, _, _, _ , _ = env.get_expert_actions() # (num_worlds, num_agents, episode_len, action_dim)
+    road_mask = env.get_road_mask()
+    partner_mask = env.get_partner_mask()
+    device = env.device
+    
+    cont_agent_mask = env.cont_agent_mask.to(device)  # (num_worlds, num_agents)
+    alive_agent_indices = cont_agent_mask.nonzero(as_tuple=False)
+    alive_agent_num = env.cont_agent_mask.sum().item()
+    print("alive_agent_num : ", alive_agent_num)
+    
+    expert_trajectory_lst = torch.zeros((alive_agent_num, env.episode_len + 4, obs.shape[-1]), device=device)
+    expert_actions_lst = torch.zeros((alive_agent_num, env.episode_len, 3), device=device)
+    expert_valid_mask_lst = torch.zeros((alive_agent_num, env.episode_len + 4), device=device, dtype=torch.bool)
+    expert_partner_mask_lst = torch.ones((alive_agent_num, env.episode_len + 4, 127), device=device, dtype=torch.bool)
+    expert_road_mask_lst = torch.ones((alive_agent_num, env.episode_len + 4, 200), device=device, dtype=torch.bool)
+    expert_global_pos_lst = torch.zeros((alive_agent_num, env.episode_len, 2), device=device) # global pos (2)
+    expert_global_rot_lst = torch.zeros((alive_agent_num, env.episode_len, 1), device=device) # global actions (1)
+
+    # Initialize dead agent mask
+    agent_info = (
+            env.sim.absolute_self_observation_tensor()
+            .to_torch()
+            .to(device)
+        )
+    dead_agent_mask = ~env.cont_agent_mask.clone().to(device) # (num_worlds, num_agents)
+    road_mask = env.get_road_mask()
+    goal_achieved = 0
+    off_road = 0
+    veh_collision = 0
+    for time_step in tqdm(range(env.episode_len)):
+        for idx, (world_idx, agent_idx) in enumerate(alive_agent_indices):
+            if not dead_agent_mask[world_idx, agent_idx]:
+                expert_trajectory_lst[idx][time_step + 4] = obs[world_idx, agent_idx]
+                expert_actions_lst[idx][time_step] = expert_actions[world_idx, agent_idx, time_step]
+                expert_partner_mask_lst[idx][time_step + 4] = partner_mask[world_idx, agent_idx] == 2
+                expert_road_mask_lst[idx][time_step + 4] = road_mask[world_idx, agent_idx]
+                expert_global_pos_lst[idx, time_step] = agent_info[world_idx, agent_idx, 0:2]
+                expert_global_rot_lst[idx, time_step] = agent_info[world_idx, agent_idx, 7:8]
+                # expert_partner_id_lst[idx, time_step] = env.partner_ids[world_idx, agent_idx].clone()
+                # expert_ego_id_lst[idx, time_step] = agent_info[world_idx, agent_idx, -1]
+                # expert_scene_id_lst[idx, time_step] = world_idx
+            expert_valid_mask_lst[idx][time_step + 4] = (~dead_agent_mask[world_idx, agent_idx]) & ~(
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, 1]) >  0.5) | 
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, 0]) >  5) | 
+                    (torch.abs(expert_actions[world_idx, agent_idx, time_step, -1]) > 0.2)
+                )
+
+        
+        # env.step() -> gather next obs
+        env.step_dynamics(expert_actions[:, :, time_step, :])
+        dones = env.get_dones().to(device)
+        
+        dead_agent_mask = torch.logical_or(dead_agent_mask, dones)
+        obs = env.get_obs() 
+        road_mask = env.get_road_mask()
+        partner_mask = env.get_partner_mask()
+        # partner_id = env.get_partner_id().unsqueeze(-1)
+        agent_info = (
+        env.sim.absolute_self_observation_tensor()
+        .to_torch()
+        .to(device)
+        )
+        infos = env.get_infos()
+        
+        goal_achieved += infos.goal_achieved[cont_agent_mask]
+        off_road += infos.off_road[cont_agent_mask]
+        veh_collision += infos.collided[cont_agent_mask]
+        goal_achieved = torch.clamp(goal_achieved, max=1.0)
+        off_road = torch.clamp(off_road, max=1.0)
+        veh_collision = torch.clamp(veh_collision, max=1.0)
+
+        if (dead_agent_mask == True).all():
+            goal_rate = goal_achieved.sum().float() / cont_agent_mask.sum().float()
+            off_road_rate = off_road.sum().float() / cont_agent_mask.sum().float()
+            veh_coll_rate = veh_collision.sum().float() / cont_agent_mask.sum().float()
+            collision = (veh_collision + off_road > 0)
+            goal_mask = goal_achieved > 0
+            print(f'Offroad {off_road_rate} VehCol {veh_coll_rate} Goal {goal_rate}')
+            break
+    
+    expert_trajectory_lst = expert_trajectory_lst[goal_mask].to('cpu')
+    expert_actions_lst = expert_actions_lst[goal_mask].to('cpu')
+    expert_valid_mask_lst = expert_valid_mask_lst[goal_mask].to('cpu')
+    expert_partner_mask_lst = expert_partner_mask_lst[goal_mask].to('cpu')
+    expert_road_mask_lst = expert_road_mask_lst[goal_mask].to('cpu')
+    # global pos
+    expert_global_pos_lst = expert_global_pos_lst[goal_mask].to('cpu')
+    expert_global_rot_lst = expert_global_rot_lst[goal_mask].to('cpu')
+
+    # make directories
+    os.makedirs(save_path + "/obs", exist_ok=True)
+    os.makedirs(save_path + "/actions", exist_ok=True)
+    os.makedirs(save_path + "/valid_mask", exist_ok=True)
+    os.makedirs(save_path + "/partner_mask", exist_ok=True)
+    os.makedirs(save_path + "/road_mask", exist_ok=True)
+    os.makedirs(save_path + "/ego_global_pos", exist_ok=True)
+    os.makedirs(save_path + "/ego_global_rot", exist_ok=True)
+
+    # save as npy
+    np.save(f"{save_path}/obs/trajectory_{save_index}.npy", expert_trajectory_lst)
+    np.save(f"{save_path}/actions/trajectory_{save_index}.npy", expert_actions_lst)
+    np.save(f"{save_path}/valid_mask/trajectory_{save_index}.npy", expert_valid_mask_lst)
+    np.save(f"{save_path}/partner_mask/trajectory_{save_index}.npy", expert_partner_mask_lst)
+    np.save(f"{save_path}/road_mask/trajectory_{save_index}.npy", expert_road_mask_lst)
+    np.save(f"{save_path}/ego_global_pos/trajectory_{save_index}.npy", expert_global_pos_lst)
+    np.save(f"{save_path}/ego_global_rot/trajectory_{save_index}.npy", expert_global_rot_lst)
+
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_stack', type=int, default=1)
-    parser.add_argument('--save_path', type=str, default='/data/full_version/processed/final2')
-    parser.add_argument('--dataset', type=str, default='validation', choices=['training', 'validation', 'testing'],)
-    parser.add_argument('--function', type=str, default='save_trajectory', 
-                        choices=[
-                            'save_trajectory'])
-    parser.add_argument('--dataset-size', type=int, default=10000) # total_world
+    parser.add_argument('--save_path', type=str, default='/data/full_version/processed')
+    parser.add_argument('--dataset', type=str, default='training', choices=['training', 'validation', 'testing'],)
+    parser.add_argument('--function', type=str, default='save_trajectory_npy', choices=['save_trajectory', 'save_trajectory_npy'])
+    parser.add_argument('--dataset-size', type=int, default=80000) # total_world
     parser.add_argument('--batch-size', type=int, default=100) # num_world
     parser.add_argument('--start-idx', type=int, default=None, help="start scene number of dataset")
     args = parser.parse_args()
@@ -175,6 +288,8 @@ if __name__ == "__main__":
         print(env.data_batch)
         if args.function == 'save_trajectory':
             save_trajectory(env, save_path, i * args.batch_size)
+        elif args.function == 'save_trajectory_npy':
+            save_trajectory_npy(env, save_path, i * args.batch_size)
         else:
             raise ValueError("Invalid function name")
         if i != total_iter - 1:
