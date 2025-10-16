5bee089830abded2683665ec7a708afd5f4f91f0
Hyeonchang Jeon <kevinjeon119@gmail.com>
2025-10-16 15:54:12 +0900

    Update intervention part (#179)
    * Update intervention.py
    
    * Update intervention part

--- gpudrive/integrations/il/figure/intervention.sh
+++ gpudrive/integrations/il/figure/intervention.sh
@@ -3,7 +3,7 @@
 # Bash Script for Running WandB Agent with GPU ID, Sweep ID, and WandB API Key
 # Usage: ./partner_ratio.sh 1 2 3 4
 
-INTERVENTION=("original" "intervention")
+INTERVENTION=("intervention" "original")
 # 반복문 실행
 for i in "${!INTERVENTION[@]}"; do
   MODE="${INTERVENTION[$i]}"
