c1483f28b4234b8d08a6c5d9490e2d2684f7b8bf
KKGB <kyungbeom8@naver.com>
2025-10-23 06:50:52 +0000

    Add: agent index viz

--- gpudrive/visualize/core.py
+++ gpudrive/visualize/core.py
@@ -1380,32 +1380,32 @@ class MatplotlibVisualizer:
         id_tensor = agent_states.id[env_idx]
         x_tensor = agent_states.pos_x[env_idx]
         y_tensor = agent_states.pos_y[env_idx]
-        # for agent_id, x, y in zip(id_tensor, x_tensor, y_tensor):
-        #     if agent_id < 0:
-        #         continue
-        #     if self.render_3d:
-        #         ax.text(
-        #             x.item(),
-        #             y.item(),
-        #             self.vehicle_height,
-        #             str(int(agent_id.item())),
-        #             ha="center",
-        #             va="center",
-        #             fontsize=6 * marker_size_scale,
-        #             color="black",
-        #             zorder=10,
-        #         )
-        #     else:
-        #         ax.text(
-        #             x.item(),
-        #             y.item(),
-        #             str(int(agent_id.item())),
-        #             ha="center",
-        #             va="center",
-        #             fontsize=6 * marker_size_scale,
-        #             color="black",
-        #             zorder=10,
-        #         )
+        for agent_idx, (agent_id, x, y) in enumerate(zip(id_tensor, x_tensor, y_tensor)):
+            if agent_id < 0:
+                continue
+            if self.render_3d:
+                ax.text(
+                    x.item(),
+                    y.item(),
+                    self.vehicle_height,
+                    str(agent_idx),
+                    ha="center",
+                    va="center",
+                    fontsize=20 * marker_size_scale,
+                    color="black",
+                    zorder=10,
+                )
+            else:
+                ax.text(
+                    x.item(),
+                    y.item(),
+                    str(agent_idx),
+                    ha="center",
+                    va="center",
+                    fontsize=20 * marker_size_scale,
+                    color="black",
+                    zorder=10,
+                )
 
     def _plot_expert_trajectories(
         self,
